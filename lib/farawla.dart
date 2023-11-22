d46cd8a4db1fb66791945e9fa0e4bb1a141fe8a4
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-22 20:27:37 +0100

    Create farawla.dart

---
+++ lib/farawla.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Farawla extends StatefulWidget {
+  const Farawla({super.key});
+
+  @override
+  State<Farawla> createState() => _FarawlaState();
+}
+
+class _FarawlaState extends State<Farawla> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
