135b0b8060815fc82ce2eb5f68f00f7c90786f9b
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-22 19:50:04 +0100

    Create farawels.dart

---
+++ lib/farawels.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Farawels extends StatefulWidget {
+  const Farawels({super.key});
+
+  @override
+  State<Farawels> createState() => _FarawelsState();
+}
+
+class _FarawelsState extends State<Farawels> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Wrap(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
