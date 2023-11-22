5d39be7aa0a3db804fd2008311e82ab4d1c4231d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-22 21:47:00 +0100

    Update farawla_container.dart

--- lib/farawla_container.dart
+++ lib/farawla_container.dart
@@ -1,7 +1,6 @@
 import 'package:code_text_field/code_text_field.dart';
 import 'package:farawla/utils/globals.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class FarawlaContainer extends StatefulWidget {
   const FarawlaContainer({super.key});
@@ -21,7 +20,6 @@ class _FarawlaContainerState extends State<FarawlaContainer> {
     super.dispose();
   }
 
-  bool _state = false;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -29,31 +27,25 @@ class _FarawlaContainerState extends State<FarawlaContainer> {
       splashColor: transparent,
       highlightColor: transparent,
       onTap: () {},
-      onHover: (bool state) => setState(() => _state = state),
-      child: AnimatedScale(
-        duration: 500.ms,
-        scale: _state ? 1.02 : 1,
-        child: Container(
-          margin: const EdgeInsets.all(4),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: <BoxShadow>[
-              BoxShadow(color: grey, blurRadius: 5, blurStyle: BlurStyle.outer),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                decoration: const BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15))),
-                child: CodeField(controller: _codeController, maxLines: 5),
-              ),
-              const SizedBox(height: 5),
-              CodeField(controller: _descriptionController, maxLines: 3),
-              Container(),
-            ],
-          ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: <BoxShadow>[
+            BoxShadow(color: grey, blurRadius: 5, blurStyle: BlurStyle.outer),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+              child: CodeField(controller: _codeController, maxLines: 5),
+            ),
+            const SizedBox(height: 5),
+            CodeField(controller: _descriptionController, maxLines: 3),
+            Container(),
+          ],
         ),
       ),
     );
