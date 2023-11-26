f411fbe96600fc5a8d2bd490142fef7e06129aa2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-26 09:46:52 +0100

    Modified  lib/farawla_container.dart

--- lib/farawla_container.dart
+++ lib/farawla_container.dart
@@ -84,7 +84,6 @@ class _FarawlaContainerState extends State<FarawlaContainer> {
                                     isOriginalAnimation: true,
                                     buttonWidget: const Icon(FontAwesomeIcons.magnifyingGlass, size: 15),
                                     trailingWidget: IconButton(
-                                      padding: EdgeInsets.all(1),
                                       onPressed: () {},
                                       icon: const Icon(FontAwesomeIcons.x, size: 15),
                                     ),
