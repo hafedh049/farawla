91b9492e4ec33c8f9fad000ec40c46e3ce09aad3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-26 10:31:43 +0100

    Modified  lib/farawla_container.dart

--- lib/farawla_container.dart
+++ lib/farawla_container.dart
@@ -110,6 +110,7 @@ class _FarawlaContainerState extends State<FarawlaContainer> {
                                                   children: <Widget>[
                                                     const Icon(FontAwesomeIcons.codeCommit, size: 15, color: pink),
                                                     const SizedBox(width: 10),
+                                                    Text(data),
                                                   ],
                                                 ),
                                               ),
