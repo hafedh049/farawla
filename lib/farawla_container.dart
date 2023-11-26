621080cbd3d06bcb76e97ccd3de35c428a2b6055
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-26 10:45:23 +0100

    Modified  lib/farawla_container.dart

--- lib/farawla_container.dart
+++ lib/farawla_container.dart
@@ -72,62 +72,59 @@ class _FarawlaContainerState extends State<FarawlaContainer> {
                         return Container(
                           padding: const EdgeInsets.all(16),
                           height: 300,
-                          child: StatefulBuilder(
-                            builder: (BuildContext context, void Function(void Function()) _) {
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  SearchBarAnimation(
-                                    onChanged: (String text) {},
-                                    textEditingController: searchLanguageController,
-                                    isOriginalAnimation: true,
-                                    buttonWidget: const Icon(FontAwesomeIcons.magnifyingGlass, size: 15),
-                                    trailingWidget: const Icon(FontAwesomeIcons.magnifyingGlass, size: 15),
-                                    secondaryButtonWidget: const Icon(FontAwesomeIcons.x, size: 15),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: ListView.builder(
-                                      itemCount: languages.length,
-                                      itemBuilder: (BuildContext context, int index) {
-                                        bool hoverState = false;
-                                        return StatefulBuilder(
-                                          builder: (BuildContext context, void Function(void Function()) $) {
-                                            return InkWell(
-                                              highlightColor: transparent,
-                                              hoverColor: transparent,
-                                              splashColor: transparent,
-                                              onHover: (bool state) => $(() => hoverState = state),
-                                              onTap: () {
-                                                _codeKey.currentState!.setState(() => _language = allLanguages[languages[index]]!);
-                                                Navigator.pop(context);
-                                              },
-                                              child: AnimatedScale(
-                                                duration: 700.ms,
-                                                scale: hoverState ? 1.02 : 1,
-                                                child: AnimatedContainer(
-                                                  margin: const EdgeInsets.all(4),
-                                                  duration: 700.ms,
-                                                  padding: const EdgeInsets.all(8),
-                                                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: hoverState ? pink : null),
-                                                  child: Row(
-                                                    children: <Widget>[
-                                                      const Icon(FontAwesomeIcons.code, size: 15, color: pink),
-                                                      const SizedBox(width: 10),
-                                                      Text(languages[index][0].toUpperCase() + languages[index].substring(1), style: TextStyle(fontSize: hoverState ? 18 : 16, fontWeight: hoverState ? FontWeight.bold : FontWeight.w400)),
-                                                    ],
-                                                  ),
-                                                ),
-                                              ),
-                                            );
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SearchBarAnimation(
+                                onChanged: (String text) {},
+                                textEditingController: searchLanguageController,
+                                isOriginalAnimation: true,
+                                buttonWidget: const Icon(FontAwesomeIcons.magnifyingGlass, size: 15),
+                                trailingWidget: const Icon(FontAwesomeIcons.magnifyingGlass, size: 15),
+                                secondaryButtonWidget: const Icon(FontAwesomeIcons.x, size: 15),
+                              ),
+                              const SizedBox(height: 10),
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: languages.length,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    bool hoverState = false;
+                                    return StatefulBuilder(
+                                      key: ,
+                                      builder: (BuildContext context, void Function(void Function()) $) {
+                                        return InkWell(
+                                          highlightColor: transparent,
+                                          hoverColor: transparent,
+                                          splashColor: transparent,
+                                          onHover: (bool state) => $(() => hoverState = state),
+                                          onTap: () {
+                                            _codeKey.currentState!.setState(() => _language = allLanguages[languages[index]]!);
+                                            Navigator.pop(context);
                                           },
+                                          child: AnimatedScale(
+                                            duration: 700.ms,
+                                            scale: hoverState ? 1.02 : 1,
+                                            child: AnimatedContainer(
+                                              margin: const EdgeInsets.all(4),
+                                              duration: 700.ms,
+                                              padding: const EdgeInsets.all(8),
+                                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: hoverState ? pink : null),
+                                              child: Row(
+                                                children: <Widget>[
+                                                  const Icon(FontAwesomeIcons.code, size: 15, color: pink),
+                                                  const SizedBox(width: 10),
+                                                  Text(languages[index][0].toUpperCase() + languages[index].substring(1), style: TextStyle(fontSize: hoverState ? 18 : 16, fontWeight: hoverState ? FontWeight.bold : FontWeight.w400)),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
                                         );
                                       },
-                                    ),
-                                  ),
-                                ],
-                              );
-                            },
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
                         );
                       },
