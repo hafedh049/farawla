d1c6edc4e2ef04e9551a807823d8fa30ded3c3d6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 11:24:29 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -54,6 +54,8 @@ class _FarawlaState extends State<Farawla> {
           }
           if (event.isControlPressed && const <LogicalKeyboardKey>[LogicalKeyboardKey.numpadEnter, LogicalKeyboardKey.enter].contains(event.logicalKey)) {
             _screenshotController.captureAndSave((await getApplicationDocumentsDirectory()).path, fileName: '${const UuidV8().generate()}.png');
+            // ignore: use_build_context_synchronously
+            showSnack('Last Cell Is Removed Successfully', context);
           }
         }
       },
