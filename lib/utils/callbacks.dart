d763dba085884e6c4ffe6f166424118c15b47e30
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 11:21:47 +0100

    Modified  lib/utils/callbacks.dart

--- lib/utils/callbacks.dart
+++ lib/utils/callbacks.dart
@@ -1,4 +1,5 @@
 import 'package:farawla/utils/globals.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -12,4 +13,24 @@ Future<bool> load() async {
     }
   }
   return true;
+}
+
+void showSnack(String msg, BuildContext context) {
+  final snackBar = SnackBar(
+    /// need to set following properties for best effect of awesome_snackbar_content
+    elevation: 0,
+    behavior: SnackBarBehavior.floating,
+    backgroundColor: Colors.transparent,
+    content: AwesomeSnackbarContent(
+      title: 'On Snap!',
+      message: 'This is an example error message that will be shown in the body of snackbar!',
+
+      /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+      contentType: ContentType.failure,
+    ),
+  );
+
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(snackBar);
 }
