51b9d53eb8983adc15ad20b05c575cf7d4746f42
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 22:08:16 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -71,15 +71,13 @@ class _FarawlaState extends State<Farawla> {
                   const SizedBox(width: 5),
                   const Text("Farawla", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: pink)),
                   Expanded(child: MoveWindow()),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: AnimSearchBar(
-                        width: width,
-                        textController: textController,
-                        onSuffixTap: onSuffixTap,
-                        onSubmitted: onSubmitted,
-                      ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: AnimSearchBar(
+                      width: width,
+                      textController: textController,
+                      onSuffixTap: onSuffixTap,
+                      onSubmitted: onSubmitted,
                     ),
                   ),
                   Expanded(child: MoveWindow()),
