e7875b42f16ca1dcd329101322565eea885cb91c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 22:22:43 +0100

    Modified  lib/farawels.dart

--- lib/farawels.dart
+++ lib/farawels.dart
@@ -198,7 +198,7 @@ class _FarawelsState extends State<Farawels> {
             Row(
               children: <Widget>[
                 const Spacer(),
-                AnimSearchBar(width: 200, textController: _searchController, onSuffixTap: () {}, onSubmitted: (String text) {}),
+                AnimSearchBar(width: 200, textController: _searchController, onSuffixTap: () {}, onSubmitted: (String text) {}),,
                 const Spacer(),
                 Stack(
                   alignment: AlignmentDirectional.center,
