d64439929d93ed8b3f846ffae61205f0deaf66bf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 11:46:59 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -67,7 +67,7 @@ class _FarawlaState extends State<Farawla> {
                 children: <Widget>[
                   const Icon(FontAwesomeIcons.cubes, size: 10, color: pink),
                   const SizedBox(width: 5),
-                  const Text("Farawla", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: pink)),
+                  const Text("Farawla", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: pink)),
                   Expanded(child: MoveWindow()),
                   TextField(),
                   Expanded(child: MoveWindow()),
