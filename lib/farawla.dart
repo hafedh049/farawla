3df874ee376e02c25773c3a4df1db1f0f0b80bab
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 11:47:55 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -65,6 +65,7 @@ class _FarawlaState extends State<Farawla> {
             WindowTitleBarBox(
               child: Row(
                 children: <Widget>[
+                  const SizedBox(width: 5),
                   const Icon(FontAwesomeIcons.cubes, size: 15, color: pink),
                   const SizedBox(width: 5),
                   const Text("Farawla", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: pink)),
