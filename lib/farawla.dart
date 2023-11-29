c075be44cbdfb3dd33c6602bcf9eac031915c404
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 22:18:37 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -63,93 +63,89 @@ class _FarawlaState extends State<Farawla> {
         }
       },
       child: Scaffold(
-        body: Stack(
+        body: Column(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                WindowTitleBarBox(
-                  child: Row(
-                    children: <Widget>[
-                      const SizedBox(width: 10),
-                      const Icon(FontAwesomeIcons.cubes, size: 15, color: pink),
-                      const SizedBox(width: 5),
-                      const Text("Farawla", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: pink)),
-                      Expanded(child: MoveWindow()),
-                      MinimizeWindowButton(),
-                      MaximizeWindowButton(),
-                      CloseWindowButton(colors: WindowButtonColors(mouseOver: pink)),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Stack(
+            WindowTitleBarBox(
+              child: Row(
+                children: <Widget>[
+                  const SizedBox(width: 10),
+                  const Icon(FontAwesomeIcons.cubes, size: 15, color: pink),
+                  const SizedBox(width: 5),
+                  const Text("Farawla", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: pink)),
+                  Expanded(child: MoveWindow()),
+                  MinimizeWindowButton(),
+                  MaximizeWindowButton(),
+                  CloseWindowButton(colors: WindowButtonColors(mouseOver: pink)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Stack(
+                  children: <Widget>[
+                    StatefulBuilder(
+                      key: _tilesKey,
+                      builder: (BuildContext context, void Function(void Function()) _) {
+                        return Positioned.fill(
+                          child: boxes[widget.boxIndex].get("data").isEmpty
+                              ? const Center(child: Text("No Tiles Yet.", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: pink)))
+                              : Screenshot(
+                                  controller: _screenshotController,
+                                  child: ListView.builder(
+                                    padding: EdgeInsets.zero,
+                                    itemCount: boxes[widget.boxIndex].get("data").length,
+                                    itemBuilder: (BuildContext context, int index) {
+                                      return Padding(padding: const EdgeInsets.all(24), child: FarawlaContainer(data: boxes[widget.boxIndex].get("data")[index], boxIndex: widget.boxIndex, tileIndex: index));
+                                    },
+                                  ),
+                                ),
+                        );
+                      },
+                    ),
+                    Row(
                       children: <Widget>[
-                        StatefulBuilder(
-                          key: _tilesKey,
-                          builder: (BuildContext context, void Function(void Function()) _) {
-                            return Positioned.fill(
-                              child: boxes[widget.boxIndex].get("data").isEmpty
-                                  ? const Center(child: Text("No Tiles Yet.", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: pink)))
-                                  : Screenshot(
-                                      controller: _screenshotController,
-                                      child: ListView.builder(
-                                        padding: EdgeInsets.zero,
-                                        itemCount: boxes[widget.boxIndex].get("data").length,
-                                        itemBuilder: (BuildContext context, int index) {
-                                          return Padding(padding: const EdgeInsets.all(24), child: FarawlaContainer(data: boxes[widget.boxIndex].get("data")[index], boxIndex: widget.boxIndex, tileIndex: index));
-                                        },
-                                      ),
-                                    ),
-                            );
-                          },
+                        InkWell(
+                          highlightColor: transparent,
+                          hoverColor: transparent,
+                          splashColor: transparent,
+                          onTap: () => Navigator.pop(context),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(shape: BoxShape.circle, color: pink.withOpacity(.6)),
+                            child: const Icon(FontAwesomeIcons.chevronLeft, size: 20, color: white),
+                          ),
                         ),
-                        Row(
+                        const Spacer(),
+                        Stack(
+                          alignment: AlignmentDirectional.center,
                           children: <Widget>[
                             InkWell(
                               highlightColor: transparent,
                               hoverColor: transparent,
                               splashColor: transparent,
-                              onTap: () => Navigator.pop(context),
+                              onTap: () async {
+                                final List data = boxes[widget.boxIndex].get("data");
+                                data.add(<dynamic, dynamic>{"language": "Python", "code": "", "explication": ""});
+                                await boxes[widget.boxIndex].put("data", data);
+                                _tilesKey.currentState!.setState(() {});
+                              },
                               child: Container(
                                 width: 40,
                                 height: 40,
                                 decoration: BoxDecoration(shape: BoxShape.circle, color: pink.withOpacity(.6)),
-                                child: const Icon(FontAwesomeIcons.chevronLeft, size: 20, color: white),
+                                child: const Icon(FontAwesomeIcons.plus, size: 20, color: white),
                               ),
                             ),
-                            const Spacer(),
-                            Stack(
-                              alignment: AlignmentDirectional.center,
-                              children: <Widget>[
-                                InkWell(
-                                  highlightColor: transparent,
-                                  hoverColor: transparent,
-                                  splashColor: transparent,
-                                  onTap: () async {
-                                    final List data = boxes[widget.boxIndex].get("data");
-                                    data.add(<dynamic, dynamic>{"language": "Python", "code": "", "explication": ""});
-                                    await boxes[widget.boxIndex].put("data", data);
-                                    _tilesKey.currentState!.setState(() {});
-                                  },
-                                  child: Container(
-                                    width: 40,
-                                    height: 40,
-                                    decoration: BoxDecoration(shape: BoxShape.circle, color: pink.withOpacity(.6)),
-                                    child: const Icon(FontAwesomeIcons.plus, size: 20, color: white),
-                                  ),
-                                ),
-                                IgnorePointer(ignoring: true, child: LottieBuilder.asset("assets/add.json", width: 60, height: 60)),
-                              ],
-                            ),
+                            IgnorePointer(ignoring: true, child: LottieBuilder.asset("assets/add.json", width: 60, height: 60)),
                           ],
                         ),
                       ],
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
