ecc3e3f0a68d9b0937a0a48c21a00684b8446d10
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 11:22:11 +0100

    Modified  lib/utils/callbacks.dart

--- lib/utils/callbacks.dart
+++ lib/utils/callbacks.dart
@@ -18,15 +18,12 @@ Future<bool> load() async {
 
 void showSnack(String msg, BuildContext context) {
   final snackBar = SnackBar(
-    /// need to set following properties for best effect of awesome_snackbar_content
     elevation: 0,
     behavior: SnackBarBehavior.floating,
     backgroundColor: Colors.transparent,
     content: AwesomeSnackbarContent(
-      title: 'On Snap!',
+      title: 'Hi!',
       message: 'This is an example error message that will be shown in the body of snackbar!',
-
-      /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
       contentType: ContentType.failure,
     ),
   );
