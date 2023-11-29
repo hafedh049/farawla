bb9f5c2564ac4a57bebf9365bf6848f40ac36abd
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-29 22:13:28 +0100

    Modified  lib/farawla.dart

--- lib/farawla.dart
+++ lib/farawla.dart
@@ -82,12 +82,6 @@ class _FarawlaState extends State<Farawla> {
                     ],
                   ),
                 ),
-                AnimSearchBar(
-                  width: 200,
-                  textController: _searchController,
-                  onSuffixTap: () {},
-                  onSubmitted: (String text) {},
-                ),
               ],
             ),
             Expanded(
